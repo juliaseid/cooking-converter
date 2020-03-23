08b4f5bbdcde8cfcdc13e9b5d4994e5740996d93
Julia Seidman <juliaseid@gmail.com>
2020-03-23 15:48:03 -0700

    Add additional conversion equations

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,37 @@ function convertTeaspoon(teaspoon){
 }
 convertTeaspoon(teaspoon);
 
+var tablespoon = parseFloat(prompt("How many tablespoons does the recipe require?"));
+
+function convertTablespoon(tablespoon){
+  alert(tablespoon + " tablespoons " + " = " + 5 * 3 * tablespoon + " milliliters");
+};
+
+convertTablespoon(tablespoon);
+
+var cup = parseFloat(prompt("How many cup does the recipe require?"));
+
+function convertCup(cup){
+  alert(cup + " cup " + " = " + 16 * 3 * 5 * cup + " milliliters. ");
+};
+
+convertCup(cup);
+
+var quart = parseFloat(prompt("How many quart does the recipe require?"));
+
+function convertQuart(quart){
+  alert(quart + " quart " + " = " + 4 * 16 * 3 * 5 * quart + " milliliters. ");
+};
+
+convertQuart(quart);
+
+var gallon = parseFloat(prompt("How many gallon does the recipe require?"));
+
+function convertGallon(gallon){
+  alert(gallon + " gallon " + " = " + 4 * 4 * 16 * 3 * 5 * gallon + " milliliters. ");
+};
+
+convertGallon(gallon);
 
 //1 teaspoon = 5 milliliters
 //1 tablespoon = 3 teaspoons
