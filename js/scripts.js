91d076c7b4dfc8afe6124863921d0276b101e30e
Julia Seidman <juliaseid@gmail.com>
2020-03-23 15:22:55 -0700

    Create basic conversion equations

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var teaspoon = parseFloat(prompt("How many teaspoons does the recipes require ")) ;
+function convertTeaspoon(teaspoon){
+  alert( teaspoon+ " teaspoons" + " = " +  5 * teaspoon + " milliliters");
+}
+convertTeaspoon(teaspoon);
+
+
+//1 teaspoon = 5 milliliters
+//1 tablespoon = 3 teaspoons
+//1 cup = 16 tablespoons
+//1 quart = 4 cups
+//1 gallon = 4 quarts = 3800 milliliters
